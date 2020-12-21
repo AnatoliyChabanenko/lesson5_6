5f2ca6027cd998766948eec6e293bd4e9d95ee58
AnatoliyChabanenko <76126324+AnatoliyChabanenko@users.noreply.github.com>
2020-12-21 18:55:17 +0200

    Merge branch 'master' of https://github.com/AnatoliyChabanenko/lesson-2-
    # Conflicts:
    #	домашка/README.md

--- lesson4,3.py
+++ lesson4,3.py
@@ -1,6 +1,6 @@
 import random
 b = input('введите что-то:')
 a = len(b)
-n = random.sample(b,a)
+n = random.shuffle(b,a)
 c = ''.join (n)
 print(c)
---
+++ lesson5,1.py
@@ -0,0 +1,4 @@
+import random
+l=list(range(1000))
+n = random.sample(l,10)
+print (max (n))
