02a8540b7616e63a24ba0df091286cc6cf8ec446
AnatoliyChabanenko <76126324+AnatoliyChabanenko@users.noreply.github.com>
2020-12-26 09:38:17 +0200

    Merge branch 'master' of https://github.com/AnatoliyChabanenko/lesson5_6
    # Conflicts: 26,12
    #	домашка/README.md

--- lesson_5_2.py
+++ lesson_5_2.py
@@ -3,10 +3,8 @@
   any duplicates.
 
 Constraints: use only while loop and random module to generate numbers
-Створіть 2 списки довжиною 10 із випадковими цілими числами від 1 до 10 і створіть
-третій список, що містить загальні цілі числа між 2 початковими списками без дублікатів.
-Обмеження: використовувати лише цикл while та випадковий модуль для генерації чисел
 """
+
 import random
 x = random.sample(range(10), 10)
 y = random.sample(range(10), 10)
